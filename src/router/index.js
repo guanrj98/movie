27f01f9f77152802be845bacae78cbaa78f3aa35
AXmeng <mengyongxiang1998@163.com>
2020-11-06 20:34:34 +0800

    我的页面静态实现

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Vant from "vant"
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Vant from "vant";
 import "vant/lib/index.css"; //引入css文件
 
 Vue.use(Vant);
@@ -8,30 +8,39 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Main',
+    path: "/",
+    name: "Main",
     component: () => import("@/views/MainPage.vue"),
-  }, {
-    path: '/list',
-    name: 'List',
-    component: () => import('../views/List.vue'),
-  }, {
-    path: '/category',
-    name: 'Category',
+  },
+  {
+    path: "/list",
+    name: "List",
+    component: () => import("../views/List.vue"),
+  },
+  {
+    path: "/category",
+    name: "Category",
     component: () => import("@/views/Category.vue"),
-  }, {
-    path: '/my',
-    name: 'MyPage',
+  },
+  {
+    path: "/my",
+    name: "MyPage",
     component: () => import("@/views/MyPage.vue"),
-  }, {
-    path: '/*',
-    name: 'NoPage',
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("@/views/login.vue"),
+  },
+  {
+    path: "/*",
+    name: "NoPage",
     component: () => import("@/views/NoPage.vue"),
-  }
-]
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
